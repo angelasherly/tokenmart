be2e32006d12f20fed8ea7794935fa7459462af3
angelasherly <59503679+angelasherly@users.noreply.github.com>
2023-04-07 17:25:27 +0700

    Initialize tokenmart project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'index.html',
+    './src/**/*.{html,js}'
+  ],
+  theme: {
+    fontFamily: {
+      body: ['Poppins', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
